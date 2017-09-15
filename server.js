da3a73755de40f4febce1de841bd60d82e0b06b3
Heet1996 <heet1@live.com>
2017-09-15 12:35:53 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+const port=process.env.PORT || 3000 ;
+var app=express();
+
+app.use(express.static(__dirname+'/public'));
+app.use((req,res,next)=>{
+var now = new Date().toString();
+fs.appendFile('server.log',now+'  '+req.method+'  '+req.url+'\n');
+next();
+});
+// app.use((req,res,next)=>{
+//   res.render('maintenance',{
+//     pageTitle:"Maintenance"
+//   });
+//
+// })
+
+app.set("view engine","hbs");
+
+hbs.registerPartials(__dirname+'/views/partials');
+hbs.registerHelper("getYear",() => {return new Date().getFullYear();});
+app.get('/',(req,res)=>
+{
+  res.render('home.hbs',{
+    pageTitle:"Home Page",
+
+  });
+}
+);
+ app.get('/about',(req,res)=>{
+  res.render('about.hbs',{
+    pageTitle:"About Page",
+
+  });
+
+});
+app.listen(port,()=>{
+  console.log(`Server is up for ${port}`);
+});
